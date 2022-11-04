28f262cf4501eab361b1fbc7fe5f697f1377cf4e
beslimir <marin.beslo@gmail.com>
2022-11-04 10:52:20 +0100

    Lazy injection

--- app/src/main/java/com/beslimir/daggerhilt_practice/MyViewModel.kt
+++ app/src/main/java/com/beslimir/daggerhilt_practice/MyViewModel.kt
@@ -2,12 +2,17 @@ package com.beslimir.daggerhilt_practice
 
 import androidx.lifecycle.ViewModel
 import com.beslimir.daggerhilt_practice.domain.repository.MyRepository
+import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MyViewModel @Inject constructor(
-    private val repository: MyRepository
+    private val myRepository: Lazy<MyRepository>
 ): ViewModel() {
+
+    init {
+        myRepository.get()
+    }
 
 }
